0f8068e34929d79e5004cf4863cbb6e09282c90f
arvinthrajasekar <rajaarvinth11@gmail.com>
2022-04-11 23:51:33 +0530

    11april-5

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,8 @@ namespace SharePrices
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 
         }
 
