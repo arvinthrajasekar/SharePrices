f3fe75a2f769d7fb755de336b42b0b8cd6844d5a
arvinthrajasekar <rajaarvinth11@gmail.com>
2022-04-11 19:54:44 +0530

    11april

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SharePrices.Controllers
         }
         
        [HttpPost]
-        public JsonResult AJAXMethod(string name)
+        public ActionResult AJAXMethod(string name)
         {
             List<Shares> AllShares = new List<Shares>();
             AllShares.Add(new Shares { Name = "3M India Ltd", Price = 21145 });
@@ -23,11 +23,33 @@ namespace SharePrices.Controllers
             AllShares.Add(new Shares { Name = "Tata Power", Price = 277.8 });
             AllShares.Add(new Shares { Name = "HDFC Bank", Price = 1516.75 });
             AllShares.Add(new Shares { Name = "Zee Entertainment", Price = 284.75 });
-            var result = from r in AllShares
-                         where r.Name == name
-                         select new { Name = r.Name, Price = r.Price };
-            
-            return Json(result);
+            try
+            {
+                var result = from r in AllShares
+                             where r.Name == name
+                             select new { Name = r.Name, Price = r.Price };
+                if(result .Count() == 0)
+                {
+                    return NotFound();
+
+                }
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpGet]
+        public ActionResult GetALL()
+        {
+            List<Shares> AllShares = new List<Shares>();
+            AllShares.Add(new Shares { Name = "3M India Ltd", Price = 21145 });
+            AllShares.Add(new Shares { Name = "Aarti Drugs Ltd", Price = 519 });
+            AllShares.Add(new Shares { Name = "Tata Power", Price = 277.8 });
+            AllShares.Add(new Shares { Name = "HDFC Bank", Price = 1516.75 });
+            AllShares.Add(new Shares { Name = "Zee Entertainment", Price = 284.75 });
+            return Json(AllShares);
         }
        
         
--- Models/Shares.cs
+++ Models/Shares.cs
@@ -1,8 +1,12 @@
-﻿namespace SharePrices.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharePrices.Models
 {
     public class Shares
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
         public double Price { get; set; }
 
     }
